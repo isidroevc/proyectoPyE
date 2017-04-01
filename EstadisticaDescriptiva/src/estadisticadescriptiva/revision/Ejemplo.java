ad87bd055f09a39f049bf9b39a965c69731361ed
Tharduz-PC\Tharduz <isidroevc@gmail.com>
2017-03-31 21:45:23 -0600

    Se creó el paquete de archivos en revisión Se creó un ejemplo de cómo deben crearse los archivos en revisión.

---
+++ EstadisticaDescriptiva/src/estadisticadescriptiva/revision/Ejemplo.java
@@ -0,0 +1,27 @@
+/**
+ * Ejemplo.java
+ * 
+ * Descripción:
+ * Este archivo corresponde a un ejemplo de cómo deben realizarse los archivos
+ * de código en revisión previa a su integración a sus módulos correspondientes
+ * 
+ * Autor: Isidro Emmanuel Vásquez Cortés
+ * Fecha de elaboración: 201
+ * Fecha de revisión: //Esta fecha la proporciona Isidro Emmanuel vasquez Cortes
+ * 
+ * Estado: Sin revisar | En revision | Corrirgiendo | Integrado
+ * 
+ * Notas Adicionales: El programador debe usar la presente plantilla para
+ * documentar éste archivo.
+ */
+package estadisticadescriptiva.revision;
+
+
+public class Ejemplo {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
