c594cf5fe24108af39f9fe2cc948913e42640972
DESKTOP-GF9ISI0\Alison <isidroevc@gmail.com>
2017-04-11 12:25:08 -0500

    Revision del código de tostado, debe corregir un error

--- EstadisticaDescriptiva/src/estadisticadescriptiva/revision/Funcion.java
+++ EstadisticaDescriptiva/src/estadisticadescriptiva/revision/Funcion.java
@@ -1,24 +1,35 @@
 package estadisticadescriptiva.revision;
 
+import java.util.Random;
+
 /*Funcion o preceso para calcular la media o promedio como lo quieran decir*/
+public class Funcion {
 
-public  class Funcion{
+    
+    
+    public static void main(String[] args ){
+        //Se crean datos aleatorios de prueba.
+        double[] datos = new double[12];
+        Random aleatorios = new Random();
+        System.out.println("Creando datos de prueba aleatorios");
+        for(int i = 0, count = datos.length; i < count; i++){
+            datos[i] = Math.abs(aleatorios.nextInt()) % 25;
+            System.out.println(datos[i]);
+        }
+        System.out.println("Media aritmetica de los datos: " + CalcularMedia(datos));
+    }
+    public static double CalcularMedia(double datos[]) {
 
-public Double CalcularPromedio (double datos []){
+        /* calcular el promedio*/
+        double sumaDatos = 0;
+        for (int i = 0; i < datos.length; i++) {
+            sumaDatos = datos[i];
 
-/* calcular el promedio*/
+        }
 
-   double sumaDatos = 0;
-for (int i = 0; i < datos.length ; i++) {
-    sumaDatos = datos[i];
+        double media = sumaDatos / datos.length;
 
-}
-
-double media = sumaDatos / datos.length;
-
-return media;
-}
-
-
+        return media;
+    }
 
 }
